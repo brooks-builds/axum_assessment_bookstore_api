590fa4a2526da5980c53871f9a82183c5e5fd307
Brookzerker <brooks@brookzerker.com>
2023-08-08 08:00:14 -0600

    created query for associating book with an author

--- src/db/book_author_queries.rs
+++ src/db/book_author_queries.rs
@@ -1,3 +1,19 @@
-pub async fn associate_book_with_author() {}
+use crate::models::book_author::BookAuthor;
+use entity::book_authors::ActiveModel as BookAuthorActiveModel;
+use eyre::Result;
+use sea_orm::{ActiveModelBehavior, ActiveModelTrait, DatabaseConnection, Set};
+
+pub async fn associate_book_with_author(
+    db: &DatabaseConnection,
+    book_author: BookAuthor,
+) -> Result<()> {
+    let mut book_authors = BookAuthorActiveModel::new();
+
+    book_authors.author_id = Set(book_author.author_id);
+    book_authors.book_id = Set(book_author.book_id);
+    book_authors.save(db).await?;
+
+    Ok(())
+}
 
 pub async fn delete_book_author_association() {}
